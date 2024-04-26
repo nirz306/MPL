d32ebc905f956b4a0a266d47267d36857db6b176
Neeraj Magdum <122902068+nirz306@users.noreply.github.com>
2024-04-26 17:13:41 +0530

    Update 6_Number_conversion.asm

--- 6_Number_conversion.asm
+++ 6_Number_conversion.asm
@@ -1,138 +1,169 @@
+;
+Write 16/64 bit ALP to convert 4-digit Hex number into its equivalent BCD
+number and 5-digit BCD number into its equivalent HEX number. Make your
+program user friendly ;to accept the choice from the user for:
+(a) HEX to BCD
+(b) BCD to HEX
+(c) EXIT.
+;Display proper strings to prompt the user while accepting the input and
+displaying the result. (use of 64-bit registers is expected).
 
-;Write 64 bit ALP to convert 4-digit Hex number into its equivalent
-;BCD number and 5-digit BCD number into its equivalent HEX
-;number. Make your program user friendly to accept the choice from
-;user for:
-;a) HEX to BCD b) BCD to HEX c) EXIT
+;***************define macro **************
+%macro input 4
+mov rax,%1
+mov rdi,%2
+mov rsi,%3
+mov rdx,%4
+syscall
+%endmacro
 
+;***************.data section **************
 section .data
-    menuMsg: db "1. HEX to BCD", 0x0A
-             db "2. BCD to HEX", 0x0A
-             db "3. Exit", 0x0A
-    menuLen equ $ - menuMsg                         ;calculates the length of the menuMsg string.
+menu db 10d,13d,"***MENU***"
+db 10d,"1. Hex to BCD"
+db 10d,"2. BCD to Hex"
+db 10d,"3. Exit"
+db 10d,"Enter your choice: "
+menulen equ $-menu
+m1 db 10d,13d,"Enter 4-digit Hex number: "
+l1 equ $-m1
+m2 db 10d,13d,"Enter 5-digit BCD number: "
+l2 equ $-m2
 
-    choicePrompt: db "Enter your Choice: "           ;initialized choicePrompt message
-    choicePromptLen equ $ - choicePrompt
+m3 db 10d,13d,"Equivalent 5-digit BCD number: "
+l3 equ $-m3
+m4 db 10d,13d,"Equivalent 4-digit Hex number: "
+l4 equ $-m4
 
-    hexPrompt: db "Enter HEX number: "                ;initialized hexPrompt message
-    hexPromptLen equ $ - hexPrompt
-
-    bcdPrompt: db "Enter BCD number: "                ;initialized badPrompt message
-    bcdPromptLen equ $ - bcdPrompt
-
-    bcdResultMsg: db "Equivalent BCD number is: "        ;initialized bcdResult message
-    bcdResultLen equ $ - bcdResultMsg
-
-    hexResultMsg: db "Equivalent HEX number is: "
-    hexResultLen equ $ - hexResultMsg
-
-    newLine: db " ", 0x0A ;used for newline
-    newLineLen equ $ - newLine
-
-section .bss                                            ;bss section allocates memory for variables that the program will use during its execution. These variables are uninitialized at the start of the program and may be filled with values as the program runs
-    number: resb 6                                       ;resb: reserves byte 
-    result: resb 4
-    answer: resb 4
-    digitCount: resb 01
-    userChoice: resb 02
-
-section .text
+section .bss
+choice resb 1
+num resb 5
+answer resb 16
+; factor resb 4
+section .code
 global _start
 _start:
+input 1,1,menu,menulen
+input 0,0,choice,2
+cmp byte[choice],'3'
+jae exit
+cmp byte[choice],'1'
+je hex2bcd
+cmp byte[choice],'2'
+je bcd2hex
 
-menu:
-    ; Display menu options            
-    mov rax, 1                            ; read instruction
-    mov rdi, 1                            ; stdin file descriptor
-    mov rsi, newLine                      ; memory location where the input is to be stored
-    mov rdx, newLineLen                   ; number of bytes to be read (16 bytes for 64 bit hexadecimal number and 1 byte for newline character)
-    syscall                               ; system call to read the input
+;**********Hex to BCD Conversion******************
+hex2bcd:
+input 1,1,m1,l1
+input 0,0,num,5
+call asciihextohex
 
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, newLine  
-    mov rdx, newLineLen
-    syscall
+mov rax,rbx
+mov rbx,10
+mov rdi,num+15
+loop3:
+mov rdx,0
+div rbx
+add dl,30h
+mov [rdi],dl
+dec rdi
+cmp rax,0
+jne loop3
+input 1,1,m3,l3
+input 1,1,num,16
+jmp _start
 
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, menuMsg        
-    mov rdx, menuLen
-    syscall                            ;system call is made to print the menu msg(different options)
+;**********BCD to Hex Conversion*************************
+bcd2hex:
+input 1,1,m2,l2
+input 0,0,num,6
+mov rbp,5
+mov rsi,num
+mov rbx,10
 
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, choicePrompt
-    mov rdx, choicePromptLen
-    syscall
+nxt4: xor rcx,rcx
+mul rbx
+mov cx,[rsi]
+sub cx,30h
+add rax,rcx
 
-    mov rax, 0
-    mov rdi, 0
-    mov rsi, userChoice
-    mov rdx, 02
-    syscall
+inc rsi
 
-    cmp byte[userChoice], 31H    //userChoice madhli value la 31H(ASCI eqivalent 1) sobat compare karta
-    je case1
+dec rbp
+jnz nxt4
+mov [ answer],rax
+input 1,1,m4,l4
+mov rax,[ answer]
+call display
+jmp _start
+exit:
+mov rax,60
+mov rdx,0
+syscall
 
-    cmp byte[userChoice], 32H
-    je case2
+;*********************PROCEDURES*************************
 
-    cmp byte[userChoice], 33H
-    je case3
+asciihextohex:                ;returns the asccii string to equivalent hexadecimal value
+mov rsi,num
+mov rcx,4
+xor rbx,rbx
+mov rax,0
 
-case2:
-    ; BCD to HEX conversion
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, bcdPrompt
-    mov rdx, bcdPromptLen
-    syscall
+loop1: rol rbx,04
+mov al,[rsi]
+cmp al,39h
+jbe skip1
+sub al,07h
+skip1:sub al,30h
+add rbx,rax
+inc rsi
+dec rcx
+jnz loop1
 
-    mov rax, 0
-    mov rdi, 0
-    mov rsi, number
-    mov rdx, 6
-    syscall
+ret
 
-    xor rax, rax
-    mov rbx, 10
-    mov rcx, 5
+display:
+mov rsi,answer+3
+mov rcx,4
+loop2:mov rdx,0
+mov rbx,16
+div rbx
+cmp dl,09h
+jbe skip2
+add dl,07h
+skip2: add dl,30h
+mov [rsi],dl
+dec rsi
+dec rcx
+jnz loop2
+input 1,1,answer,16
+ret
 
-bcdToHexLoop:
-    xor rdx, rdx        ; Clear rdx register (used for intermediate calculations)
-    mul ebx             ; Multiply rax by ebx (eax = eax * ebx), result stored in rdx:eax
+**************************************************************
+;Output Of 64 Bit NASM Code:
+$ nasm -f elf64 mil3.asm
+$ ld -o a mil3.o
+$ ./a
 
-    xor rdx, rdx        ; Clear rdx register again
-    mov dl, [rsi]       ; Load the byte at the memory address pointed to by rsi into dl
-    sub dl, 30H         ; Subtract 30H (48 in decimal) from dl to convert ASCII digit to numeric value
+MENU
+1. Hex to BCD
+2. BCD to Hex
+3. Exit
 
-    add rax, rdx       ; Add the numeric value in dl to rax (accumulator)
+Enter your choice: 1
+Enter 4-digit Hex number: FFFF
+Equivalent 5-digit BCD number: 65535
+MENU
+1. Hex to BCD
+2. BCD to Hex
+3. Exit
+Enter your choice: 2
+Enter BCD Number:65535
 
-    inc rsi             ; Increment rsi to point to the next byte in the input string
-    dec rcx             ; Decrement rcx (loop counter) to keep track of remaining iterations
-    jnz bcdToHexLoop    ; Jump to bcdToHexLoop if rcx is not zero (loop until rcx becomes zero)
-
-    mov [result], ax    ; Store the final result in the variable 'result' (assuming it's a memory location)
-
-    ; Output the result as a hexadecimal number
-    mov rax, 1          ; System call number 1 (write)
-    mov rdi, 1          ; File descriptor 1 (stdout)
-    mov rsi, hexResultMsg ; Address of the message to be printed
-    mov rdx, hexResultLen ; Length of the message
-    syscall             ; Perform the system call to print the message
-
-    mov ax, [result]    ; Load the result back into ax (assuming it's a 16-bit value)
-    call displayNumber  ; Call a subroutine to display the number
-
-    jmp menu            ; Jump to the 'menu' label (assuming it exists)
-
-
-case1:
-    ; HEX to BCD conversion
-    mov rax,
-
-
-;Test Case 1:
-;BCD - 4391
-;HEX - ABCD
+Equivalent Hex Number:FFFF
+MENU
+1. Hex to BCD
+2. BCD to Hex
+3. Exit
+Enter your choice: 3
+$
